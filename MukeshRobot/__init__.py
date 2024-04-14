6c156b17dc6e751935edc19b46bbce1737685475
biharigwaar <166360004+biharigwaar@users.noreply.github.com>
2024-04-14 16:05:23 +0530

    Update __init__.py
    Signed-off-by: biharigwaar <166360004+biharigwaar@users.noreply.github.com>

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -66,8 +66,8 @@ if ENV:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
     try:
-        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "2145093972").split())
+        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "7000670346").split())
+        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "7000670346").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
